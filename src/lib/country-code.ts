1aa89fa853ddd04ee8a4ee88689448e8c721ad6a
SergioGarcia00 <sgbsabio@gmail.com>
2026-09-07 22:14:32 +0200

    Exclude obsolete country aliases from flag URLs

--- src/lib/country-code.ts
+++ src/lib/country-code.ts
@@ -1,10 +1,7 @@
 const names = new Intl.DisplayNames(['en'], { type: 'region' });
-const codes = new Map<string, string>();
-for (let a = 65; a <= 90; a++) for (let b = 65; b <= 90; b++) {
-  const code = String.fromCharCode(a, b);
-  const name = names.of(code);
-  if (name && name !== code) codes.set(name.toLowerCase(), code.toLowerCase());
-}
+// Current ISO 3166-1 codes only: Intl also accepts obsolete aliases such as FX.
+const regions = 'AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW'.split(' ');
+const codes = new Map(regions.map(code => [names.of(code)!.toLowerCase(), code.toLowerCase()]));
 codes.set('turkey', 'tr');
 codes.set('south korea', 'kr');
 codes.set('czech republic', 'cz');
